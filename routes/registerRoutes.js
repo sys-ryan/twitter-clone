97acaa3c44b9de5228f71a25a0f83e8143aa6107
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-06-03 19:24:54 +0900

    feat: username, email validation

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const route = express.Router();
 
+const User = require("../models/User");
+
 route.get("/", (req, res, next) => {
   res.status(200).render("register");
 });
 
-route.post("/", (req, res, next) => {
+route.post("/", async (req, res, next) => {
   const firstName = req.body.firstName.trim();
   const lastName = req.body.lastName.trim();
   const username = req.body.username.trim();
@@ -15,10 +17,27 @@ route.post("/", (req, res, next) => {
 
   const payload = req.body;
 
-  if (firstName && lastName && username && email && password) {
-  } else {
-    payload.errorMessage = "Make sure each field has a valid value.";
-    res.status(200).render("register", payload);
+  try {
+    if (firstName && lastName && username && email && password) {
+      const user = await User.findOne({ $or: [{ username }, { email }] });
+
+      if (!user) {
+        // No user found
+      } else {
+        // User found
+        if (email == user.email) {
+          payload.errorMessge = "Email already in use.";
+        } else {
+          payload.errorMessage = "username already in user";
+        }
+        res.status(200).render("register", payload);
+      }
+    } else {
+      payload.errorMessage = "Make sure each field has a valid value.";
+      res.status(200).render("register", payload);
+    }
+  } catch (err) {
+    console.log(err);
   }
 });
 
