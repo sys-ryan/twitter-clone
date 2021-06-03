113f880452cffd82d819f2858eec7bf81126c35e
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-06-03 18:59:22 +0900

    feat: set database `

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require("path");
 const express = require("express");
 require("dotenv").config();
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 require("./db/mongoose");
 
 const middleware = require("./middleware");
--- db/mongoose.js
+++ db/mongoose.js
@@ -1,11 +1,24 @@
 const mongoose = require("mongoose");
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("useFindAndModify", false);
 
-mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-  })
-  .then((result) => {
-    console.log("DB Connection Successful");
-  })
-  .catch((err) => console.log(err));
+class Database {
+  constructor() {
+    this.connect();
+  }
+
+  connect() {
+    mongoose
+      .connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      })
+      .then((result) => {
+        console.log("DB Connection Successful");
+      })
+      .catch((err) => console.log(err));
+  }
+}
+
+module.exports = new Database();
