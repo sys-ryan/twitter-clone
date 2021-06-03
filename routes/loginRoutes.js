394cc2e1e1185ac8f4855d003de115cf39659fca
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-06-03 20:31:19 +0900

    feat: user login 구현

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,9 +1,35 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/User");
 
 const route = express.Router();
 
 route.get("/", (req, res, next) => {
   res.status(200).render("login");
+});
+
+route.post("/", async (req, res, next) => {
+  const username = req.body.logUsername;
+  const password = req.body.logPassword;
+
+  const payload = req.body;
+
+  if (req.body.logUsername && req.body.logPassword) {
+    const user = await User.findOne({
+      $or: [{ username }, { email: username }],
+    });
+
+    if (user) {
+      const result = await bcrypt.compare(password, user.password);
+      if (result) {
+        req.session.user = user;
+        return res.redirect("/");
+      }
+    }
+  }
+  payload.errorMessage = "Login credentials incorrect!";
+  res.status(200).render("login", payload);
 });
 
 module.exports = route;
