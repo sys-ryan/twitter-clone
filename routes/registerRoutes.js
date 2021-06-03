d65b2bc92f64dd61f86ed4feabc67db9cfc3a5f5
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-06-03 13:04:17 +0900

    feat: FE register
    - register 화면
    - BE register input validation

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -7,7 +7,19 @@ route.get("/", (req, res, next) => {
 });
 
 route.post("/", (req, res, next) => {
-  console.log(req.body);
+  const firstName = req.body.firstName.trim();
+  const lastName = req.body.lastName.trim();
+  const username = req.body.username.trim();
+  const email = req.body.email.trim();
+  const password = req.body.password;
+
+  const payload = req.body;
+
+  if (firstName && lastName && username && email && password) {
+  } else {
+    payload.errorMessage = "Make sure each field has a valid value.";
+    res.status(200).render("register", payload);
+  }
 });
 
 module.exports = route;
