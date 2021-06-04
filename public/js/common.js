ef5857d8424676a4d712f28086cab72d3f7d34ed
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-06-04 21:11:13 +0900

    debug: debug submitPostButton disabled property

--- public/js/common.js
+++ public/js/common.js
@@ -2,5 +2,12 @@ $("#postTextarea").keyup((event) => {
   const textbox = $(event.target);
   const value = textbox.val().trim();
 
-  console.log(value);
+  const submitButton = $("#submitPostButton");
+
+  if (value == "") {
+    submitButton.prop("disabled", true);
+    return;
+  }
+
+  submitButton.prop("disabled", false);
 });
