131cc3fe18eb5c0c8ec1f5b277eb3ffce9c173b7
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-06-05 19:54:35 +0900

    feat: inserting post into database 구현

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,12 +2,30 @@ const express = require("express");
 
 const route = express.Router();
 
+const Post = require("../../models/Post");
+
 route.get("/", (req, res, next) => {});
 
 route.post("/", async (req, res, next) => {
   if (!req.body.content) {
     console.log("Content param not sent with requesrt");
     return res.sendStatus(400);
+  }
+
+  const postData = {
+    content: req.body.content,
+    postedBy: req.session.user,
+  };
+
+  try {
+    let newPost = await Post.create(postData);
+    newPost = await newPost.populate("postedBy").execPopulate();
+
+    res.status(201).send(newPost);
+  } catch (err) {
+    const error = new Error("Could not create a new post");
+    error.statusCode = 400;
+    next(error);
   }
 
   res.status(200).send("It worked");
