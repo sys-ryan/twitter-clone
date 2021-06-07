3e5e4af892e1c3233831da352991930f2252dc91
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-06-08 06:30:05 +0900

    feat: update User, Post Schema for likes

--- models/Post.js
+++ models/Post.js
@@ -14,6 +14,10 @@ const PostSchema = new Schema(
       ref: "User",
     },
     pinned: Boolean,
+    likes: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }]
   },
   { timestamps: true }
 );
--- models/User.js
+++ models/User.js
@@ -33,6 +33,12 @@ const userSchema = new Schema(
       type: String,
       default: "/images/profilePic.jpeg",
     },
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
   },
   {
     timestamps: true,
