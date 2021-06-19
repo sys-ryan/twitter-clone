120da41d73f8691ed4fc3b2f85fc9ca2595b5bea
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-06-19 11:44:58 +0900

    feat: accessing the profile page by username

---
+++ routes/profileRoute.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/User");
+
+const route = express.Router();
+
+route.get("/", (req, res, next) => {
+  const payload = {
+    pageTitle: req.session.user.username,
+    userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
+    profileUser: req.session.user,
+  };
+
+  res.status(200).render("profilePage", payload);
+});
+
+route.get("/:username", async (req, res, next) => {
+  const payload = await getPayload(req.params.username, req.session.user);
+
+  res.status(200).render("profilePage", payload);
+});
+
+const getPayload = async (username, userLoggedIn) => {
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return {
+      pageTitle: "User not found",
+      userLoggedIn,
+      userLoggedInJs: JSON.stringify(userLoggedIn),
+    };
+  }
+
+  return {
+    pageTitle: user.username,
+    userLoggedIn,
+    userLoggedInJs: JSON.stringify(userLoggedIn),
+    profileUser: user,
+  };
+};
+
+module.exports = route;
