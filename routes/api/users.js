a073ada32f1982163d071ab1a5fb114ca91c6198
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-06-22 13:52:52 +0900

    feat: following and unfollowing 구현

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,15 +7,34 @@ const User = require("../../models/User");
 route.put("/:userId/follow", async (req, res, next) => {
   const userId = req.params.userId;
 
-  const user = await User.findById(userId);
+  try {
+    const user = await User.findById(userId);
 
-  if (user == null) {
-    return res.sendStatus(404);
+    if (user == null) {
+      return res.sendStatus(404);
+    }
+
+    const isFollowing =
+      user.followers && user.followers.includes(req.session.user._id);
+    const option = isFollowing ? "$pull" : "$addToSet";
+
+    req.session.user = await User.findByIdAndUpdate(
+      req.session.user._id,
+      { [option]: { following: userId } },
+      { new: true }
+    );
+
+    await User.findByIdAndUpdate(userId, {
+      [option]: { followers: req.session.user._id },
+    });
+
+    res.status(200).send(req.session.user);
+  } catch (err) {
+    if (!err.statudCode) {
+      err.statusCode = 500;
+    }
+    next(err);
   }
-
-  const isFollowing =
-    user.followers && user.followers.includes(req.session.user._id);
-  res.status(200).send(isFollowing);
 });
 
 module.exports = route;
