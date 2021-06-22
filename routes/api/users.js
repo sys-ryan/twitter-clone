033f62ca313b1b1e8b098139420ef40f590de44c
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-06-22 13:43:50 +0900

    feat: following check 구현

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,20 @@ const express = require("express");
 
 const route = express.Router();
 
+const User = require("../../models/User");
+
 route.put("/:userId/follow", async (req, res, next) => {
-  res.status(200).send("follow!");
+  const userId = req.params.userId;
+
+  const user = await User.findById(userId);
+
+  if (user == null) {
+    return res.sendStatus(404);
+  }
+
+  const isFollowing =
+    user.followers && user.followers.includes(req.session.user._id);
+  res.status(200).send(isFollowing);
 });
 
 module.exports = route;
