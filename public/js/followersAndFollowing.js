699bf6d8a8cb280ac3ced1a1d9018269ce162e1a
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-06-22 20:48:06 +0900

    feat: outputting the users

--- public/js/followersAndFollowing.js
+++ public/js/followersAndFollowing.js
@@ -8,16 +8,42 @@ $(document).ready(() => {
 
 const loadFollowers = async () => {
   $.get(`/api/users/${profileUserId}/followers`, (results) => {
-    outputUsers(results, $(".resultsContainer"));
+    outputUsers(results.followers, $(".resultsContainer"));
   });
 };
 
 const loadFollowing = async () => {
   $.get(`/api/users/${profileUserId}/following`, (results) => {
-    outputUsers(results, $(".resultsContainer"));
+    outputUsers(results.following, $(".resultsContainer"));
   });
 };
 
-const outputUsers = (data, container) => {
-  console.log(data);
+const outputUsers = (results, container) => {
+  container.html("");
+
+  results.forEach((result) => {
+    let html = createUserHtml(result, true);
+    container.append(html);
+  });
+
+  if (results.length === 0) {
+    container.append(`<span class='noResults'>No results found</span>`);
+  }
+};
+
+const createUserHtml = (userData, showFollowButton) => {
+  const name = `${userData.firstName} ${userData.lastName}`;
+  return `
+    <div class='user'>
+      <div class='userImageContainer'>
+        <img src='${userData.profilePic}'/>
+      </div>
+      <div class='userDetailsContainer'>
+        <div class='header'>
+          <a href='/profile/${userData.username}'>${name}</a>
+          <span class='username'>@${userData.username}</span>
+        </div>
+      </div>
+    </div>
+  `;
 };
