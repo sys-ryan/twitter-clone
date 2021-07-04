267a073d2e2d433c1b21142c0f3f80993d0689db
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-07-05 00:16:45 +0900

    feat: creating chats

--- routes/api/chats.js
+++ routes/api/chats.js
@@ -2,6 +2,36 @@ const express = require("express");
 
 const route = express.Router();
 
-route.post("/", async (req, res, next) => {});
+const Chat = require("../../models/Chat");
+
+route.post("/", async (req, res, next) => {
+  if (!req.body.users) {
+    console.log("Users parm not sent with request");
+    return res.sendStatus(400);
+  }
+
+  const users = JSON.parse(req.body.users);
+  if (users.length == 0) {
+    console.log("Users array is empty");
+    return res.sentStatus(400);
+  }
+
+  users.push(req.session.user);
+
+  let chatData = {
+    users,
+    isGroupChat: true,
+  };
+
+  try {
+    const results = await Chat.create(chatData);
+    res.status(200).send(results);
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+});
 
 module.exports = route;
