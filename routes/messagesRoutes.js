1325982d10a1ae64fa649def401b7a275ce7bcb5
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-07-05 15:46:30 +0900

    feat: getting the chat data for the page

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+const Chat = require("../models/Chat");
 const route = express.Router();
 
 route.get("/", (req, res, next) => {
@@ -20,11 +21,24 @@ route.get("/new", (req, res, next) => {
   res.status(200).render("newMessage", payload);
 });
 
-route.get("/:chatId", (req, res, next) => {
+route.get("/:chatId", async (req, res, next) => {
+  const userId = req.session.user._id;
+  const chatId = req.params.chatId;
+
+  const chat = await Chat.findOne({
+    _id: chatId,
+    users: { $elemMatch: { $eq: userId } },
+  }).populate("users");
+
+  if (chat == null) {
+    // Check if chat id is really user id
+  }
+
   let payload = {
     pageTitle: "Chat",
     userLoggedIn: req.session.user,
     userLoggedInJs: JSON.stringify(req.session.user),
+    chat,
   };
   res.status(200).render("chatPage", payload);
 });
