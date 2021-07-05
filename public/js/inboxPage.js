e30eac81eae832529ad1977abb6cdf0faed204e9
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-07-05 14:12:53 +0900

    feat: outputting the chat elements

--- public/js/inboxPage.js
+++ public/js/inboxPage.js
@@ -9,5 +9,27 @@ $(document).ready(() => {
 });
 
 function outputChatList(chatList, container) {
-  console.log(chatList);
+  chatList.forEach((chat) => {
+    let html = createChatHtml(chat);
+    container.append(html);
+  });
+
+  if (chatList.length == 0) {
+    container.append("<span class='noResults'>Nothing to show.</span>");
+  }
+}
+
+function createChatHtml(chatData) {
+  let chatName = "Chat name"; // TODO
+  let image = ""; // TODO
+  let latestMessage = "This is the latest message";
+
+  return `
+    <a href="/messages/${chatData._id}" class="resultListItem">
+      <div class="resultsDetailsContainer">
+        <span class="heading">${chatName}</span>
+        <span class="subText">${latestMessage}</span>
+      </div>
+    </a>
+  `;
 }
