d7fad89af0ed2078fd2748f6692100d922f694dd
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-07-12 21:28:09 +0900

    feat: get chat data by chat ID
    - set chat name of the chat page

--- public/js/common.js
+++ public/js/common.js
@@ -636,3 +636,25 @@ function updateSelectedUserHtml() {
   $(".selectedUser").remove();
   $("#selectedUsers").prepend(elements);
 }
+
+function getChatName(chatData) {
+  let chatName = chatData.chatName;
+
+  if (!chatName) {
+    const otherChatUsers = getOtherChatUsers(chatData.users);
+    const namesArray = otherChatUsers.map(
+      (user) => `${user.firstName} ${user.lastName}`
+    );
+    chatName = namesArray.join(", ");
+  }
+
+  return chatName;
+}
+
+function getOtherChatUsers(users) {
+  if (users.length == 1) {
+    return users;
+  }
+
+  return users.filter((user) => user._id !== userLoggedIn._id);
+}
