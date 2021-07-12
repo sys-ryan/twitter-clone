2c6cc590ec0fba44f93af5018b9d7af85864e134
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-07-12 20:56:58 +0900

    feat: Making the ajax call to change the chat name

---
+++ public/js/chatPage.js
@@ -0,0 +1,15 @@
+$("#chatNameButton").click(() => {
+  const name = $("#chatNameTextbox").val().trim();
+  $.ajax({
+    url: `/api/chat/${chatId}`,
+    type: "PUT",
+    data: { chatName: name },
+    success: (data, status, xhr) => {
+      if (xhr.status !== 204) {
+        alert("could not update");
+      } else {
+        location.reload();
+      }
+    },
+  });
+});
