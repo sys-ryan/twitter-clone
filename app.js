e2870ff887ef3979aad061d3184abaac5ea720e4
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-07-21 16:13:40 +0900

    feat: Sending a new message event

--- app.js
+++ app.js
@@ -79,4 +79,16 @@ io.on("connection", (socket) => {
 
   socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  socket.on("new message", (newMessage) => {
+    let chat = newMessage.chat;
+    if (!chat.users) {
+      return console.log("Chat.users not defined");
+    }
+
+    chat.users.forEach((user) => {
+      if (usre._id == newMessage.sender._id) return;
+
+      socket.in(user._id).emit("message received", newMessage);
+    });
+  });
 });
